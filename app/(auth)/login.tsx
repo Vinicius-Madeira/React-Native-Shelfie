7a685c2d74d1fb7028c73e1d200be45fe27220a2
Vinicius Madeira <viniciussmadeira@gmail.com>
2025-04-21 03:31:10 -0300

    add: login and register pages

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,16 +1,25 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Text } from "react-native";
+import { Link } from "expo-router";
+import { Colors } from "../../constants/Colors";
 import ThemedView from "../../components/ThemedView";
 import ThemedText from "../../components/ThemedText";
 import Spacer from "../../components/Spacer";
-import { Link } from "expo-router";
+import ThemedButton from "../../components/ThemedButton";
 
 const Login = () => {
+  function handleSubmit() {
+    console.log("login form submitted");
+  }
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title style={styles.title}>
         Login to Your Account
       </ThemedText>
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: "#f2f2f2" }}>Login</Text>
+      </ThemedButton>
 
       <Spacer height={100} />
       <Link href="/register">
@@ -28,10 +37,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center",
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30,
+  },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8,
   },
 });
